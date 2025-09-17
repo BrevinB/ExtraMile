a5c2f90f8c58c1c338ef75572da576da95d17124
Brevin Blalock <bblalock313@gmail.com>
2025-09-17 17:39:19 -0400

    Respect safe areas in dashboard

--- ExtraMile+/ContentView.swift
+++ ExtraMile+/ContentView.swift
@@ -73,7 +73,6 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("ExtraMile")
         }
-        .ignoresSafeArea(.all)
         .sheet(isPresented: $showGoalCreator) {
             GoalEditorView()
         }
